ec874fcce2bbdfd035c4dda8878edec49449d5aa
Georgii Kvelidze <gi.kvelidze@storige.ru>
2020-12-15 11:56:16 +0300

    Реализована утилита tree

--- main.go
+++ main.go
@@ -2,32 +2,72 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path"
 	"sort"
-	"strings"
 )
 
-func dirTree(out *os.File, path string, printFiles bool) (err error) {
-	file, err := os.Open(path)
+func dirTree(out io.Writer, path string, printFiles bool) (err error) {
+	recursiveDir(out, path, printFiles, "")
+	return nil
+}
+
+func getPrintInfo(out io.Writer, v os.FileInfo, arrayLen int, isLastPosition bool, prefix string) {
+	usualPrefix := "├───"
+	lastPositionPrefix := "└───"
+	size := ""
+	tabPrefix := ""
+
+	if isLastPosition {
+		tabPrefix = prefix + lastPositionPrefix
+	} else {
+		tabPrefix = prefix + usualPrefix
+	}
+
+	if !v.IsDir() {
+		if v.Size() == 0 {
+			size = " (empty)"
+		} else {
+			size = fmt.Sprintf(" (%db)", v.Size())
+		}
+	}
+	fmt.Fprintf(out, "%s%s%s\n", tabPrefix, v.Name(), size)
+}
+
+func recursiveDir(out io.Writer, pathDir string, printFiles bool, prefix string) {
+	file, _ := os.Open(pathDir)
 	files, _ := file.Readdir(-1)
-	prefix := "├───"
-	level := 0
-	tabPrefix := strings.Repeat("\t", level) + prefix
+	newPrefix := ""
+	var newFiles []os.FileInfo
+	file.Close()
 	sort.Slice(files, func(i, j int) bool {
 		return files[i].Name() < files[j].Name()
 	})
-	for _, v := range files {
-		size := ""
-		if !v.IsDir() {
-			if v.Size() == 0 {
-				size = "(empty)"
-			} else {
-				size = fmt.Sprintf("(%db)", v.Size())
+
+	if !printFiles {
+		for i := 0; i < len(files); i++ {
+			if files[i].IsDir() {
+				newFiles = append(newFiles, files[i])
 			}
 		}
-		fmt.Printf("%s %s %s \n", tabPrefix, v.Name(), size)
+		files = newFiles
 	}
-	return nil
+
+	for i := 0; i < len(files); i++ {
+		isLastPosition := i+1 == len(files)
+		if isLastPosition {
+			newPrefix = "\t"
+		} else {
+			newPrefix = "│\t"
+		}
+		if files[i].IsDir() {
+			getPrintInfo(out, files[i], len(files), isLastPosition, prefix)
+			recursiveDir(out, path.Join(pathDir, files[i].Name()), printFiles, prefix+newPrefix)
+		} else if printFiles {
+			getPrintInfo(out, files[i], len(files), isLastPosition, prefix)
+		}
+	}
 }
 
 func main() {
